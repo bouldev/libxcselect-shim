9ac92a6fae4fff4979a0fe442711f35c61383172
LNSSPsd <24724395+LNSSPsd@users.noreply.github.com>
2022-01-17 17:41:57 +0800

    xcselect_developer_dir_matches_path

---
+++ src/libxcselect/xcselect_developer_dir_matches_path.c
@@ -0,0 +1,29 @@
+#include <xcselect.h>
+#include <string.h>
+
+XC_EXPORT
+bool xcselect_developer_dir_matches_path(const char *developer_dir,const char *path) {
+	char str[1024];
+	if(a1) {
+		strlcpy(str,developer_dir,1024);
+	}else{
+		bool ignoringPool;
+		if(!xcselect_get_developer_dir_path(str,1024,&ignoringPool,&ignoringPool,&ignoringPool)) {
+			return false;
+		}
+	}
+	size_t len=strlen(str);
+	if(!len)return false;
+	for(int i=&str[len - 1]; i>str; *i-- = 0 ) {
+		if(*i!='/')break;
+	}
+	len=strlen(str);
+	if(str_endswith(str,".app/Contents/Developer")) {
+		str[len-19]=0;
+	}
+	len=strlen(str);
+	if(path&&strlen(path)>=len) {
+		return strncmp(str,path,len)==0;
+	}
+	return false;
+}
