af2c9c7cc3ce1ac71245623d531899365510bda4
Torrekie <me@torrekie.dev>
2022-01-17 17:58:58 +0800

    fix xcselect_developer_dir_maches_path

--- src/libxcselect/xcselect_developer_dir_matches_path.c
+++ src/libxcselect/xcselect_developer_dir_matches_path.c
@@ -4,26 +4,26 @@
 XC_EXPORT
 bool xcselect_developer_dir_matches_path(const char *developer_dir,const char *path) {
 	char str[1024];
-	if(a1) {
-		strlcpy(str,developer_dir,1024);
-	}else{
+	if (developer_dir) {
+		strlcpy(str, developer_dir, MAXPATHLEN);
+	} else {
 		bool ignoringPool;
-		if(!xcselect_get_developer_dir_path(str,1024,&ignoringPool,&ignoringPool,&ignoringPool)) {
+		if (!xcselect_get_developer_dir_path(str, 1024, &ignoringPool, &ignoringPool, &ignoringPool)) {
 			return false;
 		}
 	}
-	size_t len=strlen(str);
-	if(!len)return false;
-	for(int i=&str[len - 1]; i>str; *i-- = 0 ) {
-		if(*i!='/')break;
+	size_t len = strlen(str);
+	if (!len) return false;
+	for (char *i = &str[len - 1]; i > str; *i-- = 0 ) {
+		if (*i != '/') break;
 	}
-	len=strlen(str);
-	if(str_endswith(str,".app/Contents/Developer")) {
-		str[len-19]=0;
+	len = strlen(str);
+	if (str_endswith(str, ".app/Contents/Developer")) {
+		str[len - 19] = 0;
 	}
-	len=strlen(str);
-	if(path&&strlen(path)>=len) {
-		return strncmp(str,path,len)==0;
+	len = strlen(str);
+	if (path && strlen(path) >= len) {
+		return strncmp(str, path, len) == 0;
 	}
 	return false;
 }
--- xcselect.h
+++ xcselect.h
@@ -1,29 +1,40 @@
-#ifndef _LIBXCSELECT_H_
-#define _LIBXCSELECT_H_
+#ifndef __XCSELECT_H__
+#define __XCSELECT_H__
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/param.h>
+//#include <CoreFoundation/CFBase.h>
 
 #define XC_EXPORT __attribute__((visibility("default")))
 #define XC_HIDDEN __attribute__((visibility("hidden")))
 #define BUNDLE_PREFIX "com.apple."
 
+typedef struct xcselect_manpaths {
+	char **paths;
+	uint32_t count;
+} xcselect_manpaths;
+
 XC_EXPORT bool xcselect_bundle_is_developer_tool(char *path);
-XC_EXPORT bool xcselect_developer_dir_matches_path(const char *path_orig, char *path_cmp);
+XC_EXPORT bool xcselect_developer_dir_matches_path(const char *developer_dir, const char *path);
 XC_EXPORT bool xcselect_get_developer_dir_path(char *buffer, int buffer_size,
 				bool *was_environment, bool *was_cltools,
 				bool *was_default);
 XC_EXPORT bool xcselect_find_developer_contents_from_path(char *path, char *buffer,
 				size_t buffer_size, bool *was_cltools);
-
+XC_EXPORT xcselect_manpaths *xcselect_get_manpaths(char *sysroot);
+XC_EXPORT char *xcselect_get_version(void);
+//XC_EXPORT errno_t xcselect_host_sdk_path(/*xcselect_host_sdk_policy_t*/int sdk_policy,
+//				char * __nullable * __nonnull path);
 #ifdef _XC_INTN
-void path_append(char *path_dest, size_t length, const char *path_to_join);
-void path_join(char *path_dest, size_t length, const char *path_src, const char *path_extra);
-bool path_exists(char *path);
-bool path_contains_xcrun(const char *path);
-bool path_is_dir(const char *path);
-bool is_path_xcrun_shim(const char *path);
+XC_HIDDEN void path_append(char *path_dest, size_t length, const char *path_to_join);
+XC_HIDDEN void path_join(char *path_dest, size_t length, const char *path_src, const char *path_extra);
+XC_HIDDEN bool path_exists(char *path);
+XC_HIDDEN bool path_contains_xcrun(const char *path);
+XC_HIDDEN bool path_is_dir(const char *path);
+XC_HIDDEN bool str_endswith(const char *str, const char *end);
+XC_HIDDEN bool is_path_xcrun_shim(const char *path);
 #endif
 
 #endif
