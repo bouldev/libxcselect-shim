7db17490bf95b38b2f2e46be802d63fda0ff8ce2
Torrekie <me@torrekie.dev>
2022-01-19 22:15:26 +0800

    update header

--- xcselect.h
+++ xcselect.h
@@ -3,9 +3,12 @@
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/types.h>
 #include <sys/param.h>
+#include <dispatch/dispatch.h>
 #include <CoreFoundation/CFBase.h>
 
 #define XC_EXPORT __attribute__((visibility("default")))
@@ -49,6 +52,7 @@ XC_HIDDEN bool path_is_dir(const char *path);
 XC_HIDDEN bool str_endswith(const char *str, const char *end);
 XC_HIDDEN bool get_developer_dir_from_symlink(const char *path, char *buffer, int buffer_size, bool *status);
 XC_HIDDEN void xcselect_manpaths_append(xcselect_manpaths* paths, const char* path);
+XC_HIDDEN errno_t sdks_at_path(char *sdkdir, char * __nullable * __nonnull path, size_t length);
 XC_HIDDEN bool is_path_xcrun_shim(const char *path);
 #endif
 
