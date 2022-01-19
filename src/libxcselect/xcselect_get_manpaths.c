8ee1ebb9e31965bae80c59c98604dc2a0c3e741d
Torrekie <me@torrekie.dev>
2022-01-19 16:22:41 +0800

    fix bad practice

--- src/libxcselect/xcselect_get_manpaths.c
+++ src/libxcselect/xcselect_get_manpaths.c
@@ -9,39 +9,42 @@ xcselect_manpaths *xcselect_get_manpaths(char *sdkname)
 	bool ignored;
 	xcselect_manpaths *rv;
 
+	if (_xpc_runtime_is_app_sandboxed() && (xcselect_get_developer_dir_path(path, MAXPATHLEN, &ignored, &ignored, &ignored)) != 0)
+		return NULL;
+
 	rv = (xcselect_manpaths*) malloc(sizeof(*rv));
 	memset(rv, 0, sizeof(*rv));
-
-	if ((_xpc_runtime_is_app_sandboxed() & 1) == 0 && (xcselect_get_developer_dir_path(path, MAXPATHLEN, &ignored, &ignored, &ignored)) != 0) {
-		path_join(path_xcrun, MAXPATHLEN, path, "usr/lib/libxcrun.dylib");
-		if (path_exists(path_xcrun) != 0) {
-			void *open_xcrun = dlopen(path_xcrun, RTLD_LAZY);
-			void (*xcrun_iter_manpaths)(const char* devpath, const char* sdkname, void (^)(const char*));
-			if (!open_xcrun) {
-				free(rv);
-				fprintf(stderr, "%s: error: unable to load libxcrun (%s).\n", getprogname(), dlerror());
-				return NULL;
-			}
-
-			*((void**)&xcrun_iter_manpaths) = dlsym(open_xcrun, "xcrun_iter_manpaths");
-			if (xcrun_iter_manpaths != NULL) {
-				path[MAXPATHLEN] = 0;
-				xcrun_iter_manpaths(path, sdkname, ^(const char* path) {
-					xcselect_manpaths_append(rv, path);
-				});
-			}
-
-			dlclose(open_xcrun);
+	path_join(path_xcrun, MAXPATHLEN, path, "usr/lib/libxcrun.dylib");
+	if (path_exists(path_xcrun) != 0) {
+		void *open_xcrun = dlopen(path_xcrun, RTLD_LAZY);
+		void (*xcrun_iter_manpaths)(const char* devpath, const char* sdkname, void (^)(const char*));
+		if (!open_xcrun) {
+			free(rv);
+			fprintf(stderr, "%s: error: unable to load libxcrun (%s).\n", getprogname(), dlerror());
+			return NULL;
 		}
+
+		xcrun_iter_manpaths = dlsym(open_xcrun, "xcrun_iter_manpaths");
+		if (xcrun_iter_manpaths != NULL) {
+			path[MAXPATHLEN] = 0;
+			xcrun_iter_manpaths(path, sdkname, ^(const char* path) {
+				xcselect_manpaths_append(rv, path);
+			});
+		}
+
+		dlclose(open_xcrun);
+		free(rv);
+	} else {
+		path_join(path_to_append, MAXPATHLEN, path, "usr/share/man");
+		xcselect_manpaths_append(rv, path_to_append);
+
+		path_join(path_to_append, MAXPATHLEN, path, "usr/llvm-gcc-4.2/share/man");
+		xcselect_manpaths_append(rv, path_to_append);
+
+		path_join(path_to_append, MAXPATHLEN, path, "Toolchains/XcodeDefault.xctoolchain/usr/share/man");
+		xcselect_manpaths_append(rv, path_to_append);
+
+		free(rv);
 	}
-	path_join(path_to_append, MAXPATHLEN, path, "usr/share/man");
-	xcselect_manpaths_append(rv, path_to_append);
-
-	path_join(path_to_append, MAXPATHLEN, path, "usr/llvm-gcc-4.2/share/man");
-	xcselect_manpaths_append(rv, path_to_append);
-
-	path_join(path_to_append, MAXPATHLEN, path, "Toolchains/XcodeDefault.xctoolchain/usr/share/man");
-	xcselect_manpaths_append(rv, path_to_append);
-
 	return rv;
 }
