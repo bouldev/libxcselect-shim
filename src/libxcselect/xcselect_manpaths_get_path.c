07fca4af201ff216fb61fb27817656232ca9fa93
Torrekie <me@torrekie.dev>
2022-01-20 15:13:16 +0800

    fix typo

--- src/libxcselect/xcselect_manpaths_get_path.c
+++ src/libxcselect/xcselect_manpaths_get_path.c
@@ -4,7 +4,7 @@ XC_EXPORT
 const char *xcselect_manpaths_get_path(xcselect_manpaths *xcp, uint32_t id)
 {
 	if (idx < xcselect_manpaths_get_num_paths(xcp))
-		return xcp->paths[idx];
+		return xcp->paths[id];
 	else
 		return NULL;
 }
