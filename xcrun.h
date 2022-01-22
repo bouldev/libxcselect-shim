190628ef97bfcff55dc1756bf847c1d19be45f0f
Torrekie <me@torrekie.dev>
2022-01-22 17:49:48 +0800

    fix xcrun_main

--- xcrun.h
+++ xcrun.h
@@ -14,7 +14,7 @@ XC_EXPORT void *xcrun_iter_manpaths(const char* devpath, const char* sdkname, co
 XC_EXPORT int xcrun_main(int argc, char *argv[]);
 XC_EXPORT void *xcrun_set_unknown_utility_handler(const void *block_invoke);
 
-XC_HIDDEN int xcrun_main2(int argc, char *argv[]);
+XC_HIDDEN int xcrun_main2(char *tool_name, int argc, char *argv[], char *devdir);
 XC_HIDDEN bool util_get_bool_from_environment(const char *env);
 XC_HIDDEN void path_clean_copy(char *path);
 XC_HIDDEN char *msprintf(char *str, ...);
