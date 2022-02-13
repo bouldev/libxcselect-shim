fe7f983b63dc1c3c6820b4c79a9f51e5dca66086
LNSSPsd <24724395+LNSSPsd@users.noreply.github.com>
2022-02-13 18:04:51 +0800

    Rewrite config reading function, **UNTESTED**

--- src/libxcselect/xcselect_config.c
+++ src/libxcselect/xcselect_config.c
@@ -18,78 +18,153 @@ static char *strtolower(char *s)
     return d;
 }
 
-static bool
-xcselect_get_config(const char *key, char *value)
-{
-	static struct {
-		dispatch_once_t onceToken;
-		bool config_exists;
-		bool config_readable;
-		FILE *config_open;
-	} xcselect_config;
+static struct {
+	char ***config_entries=NULL;
+	bool config_available=true;
+} xcselect_config;
 
-	if (xcselect_config.onceToken != -1) {
-		dispatch_once(&xcselect_config.onceToken, ^{
-			xcselect_config.config_exists = path_exists(_PATH_XCSELECT_CONF);
-			if (xcselect_config.config_exists) {
-				if (xcselect_config.config_open = fopen(_PATH_XCSELECT_CONF, "r"), xcselect_config.config_open != NULL) {
-					xcselect_config.config_readable = true;
-				} else {
-					xcselect_config.config_readable = false;
-					fprintf(stderr, "Syntax error: config file %s not readable (%s)", _PATH_XCSELECT_CONF, strerror(errno));
-				}
-			} else {
-				// path_exists set errno to ENOENT if path not exist
-				fprintf(stderr, "Syntax error: config file %s not exist, using default logics (%s)\n", _PATH_XCSELECT_CONF, strerror(errno));
-			}
-		});
+static void
+xcselect_free_config() {
+	xcselect_config.config_available=true;
+	if(xcselect_config.config_entries) {
+		for(char ***item=xcselect_config.config_entries;item!=NULL;item++) {
+			char **p=*item;
+			free(p[0]);
+			free(p[1]);
+			free(p);
+		}
+		free(xcselect_config.config_entries);
+		xcselect_config.config_entries=NULL;
 	}
-	char line[MAXPATHLEN], bool_format[128];
-	bool key_is_bool, found_config_key = false;
-	char ignore_config_str[] = "Ignored config at line", syntax_err_str[] = "with syntax error:";
-	int linenum = 0;
+}
 
-	if (xcselect_config.config_readable) {
-		while(fgets(line, MAXPATHLEN, xcselect_config.config_open) != NULL)
-		{
-			char config_key[256], config_value[MAXPATHLEN];
-
-			linenum++;
-			if(line[0] == '#') continue;
-
-			if(sscanf(line, "%s %s", config_key, config_value) != 2) {
-				fprintf(stderr, "%s %d %s %s", ignore_config_str, linenum, syntax_err_str, "more than one arguments\n");
-				continue;
+static char *
+xcselect_get_config_string(const char *key)
+{
+	if (xcselect_config.config_available&&!xcselect_config.config_entries) {
+		xcselect_config.config_available = path_exists(_PATH_XCSELECT_CONF);
+		if (xcselect_config.config_available) {
+			FILE *config_file=fopen(_PATH_XCSELECT_CONF, "rb");
+			if(!config_file) {
+				xcselect_config.config_available=false;
+				return;
 			}
-
-			if (strcmp(config_key, key) == 0) {
-				// Pass config_value to bool_format for us to format input later
-				if (strcmp(key, "is_path_xcrun_shim")) {
-					key_is_bool = true;
-					strcat(bool_format, strtolower(config_value));
+			char *item_name=NULL;
+			config_entries=calloc(32,sizeof(void *));
+			int configArrSize=32;
+			int configItemIndex=0;
+			char parseBuf[1024]={0};
+			int itemIndex=0;
+			unsigned short parseIndex=0;
+			int c;
+			int lineNumber=1;
+			int isComment=0;
+			while(1) {
+				c=fgetc(config_file);
+				if(c==EOF) {
+					if(itemIndex==1) {
+						c='\n';
+					}else if(parseIndex==0) {
+						fclose(config_file);
+						if(configItemIndex==configArrSize) {
+							config_entries=reallocarray(config_entries, configArrSize++, sizeof(void*));
+						}
+						config_entries[configItemIndex]=NULL;
+						return;
+					}else{
+						fprintf(stderr, "Syntax error: config file " _PATH_XCSELECT_CONF ": early EOF at line %d.",lineNumber);
+						fclose(config_file);
+						xcselect_config.config_available=false;
+						for(int i=0;i<configItemIndex;i++) {
+							free(config_entries[i][0]);
+							free(config_entries[i][1]);
+							free(config_entries[i]);
+						}
+						if(item_name!=NULL) {
+							free(item_name);
+						}
+						return;
+					}
 				}
-
-				// Multiple config_value for one key
-				if (strcmp(key, "developer_tool_bundle_id")) {
-					strcpy(config_value, value);
-					strcat(config_value, BUNDLE_SEPERATOR);
+				if(c=='\t'||c==' '||c=='\r') {
+					continue;
 				}
-
-				// Format bool to string "true" or "false" if have any
-				if (key_is_bool) {
-					if (strcmp(bool_format, "0")) { strcpy(config_value, "false"); }
-					else if (strcmp(bool_format, "1")) { strcpy(config_value, "true"); }
-					else if (!strcmp(bool_format, "true") && !strcmp(bool_format, "false")) {
-						fprintf(stderr, "%s %d %s %s", ignore_config_str, linenum, syntax_err_str, "not a bool value\n");
+				if(c=='#') {
+					isComment++;
+					continue;
+				}
+				if(!isComment) {
+					if(itemIndex==0&&c=='=') {
+						int parseBufLen=strlen(parseBuf)+1;
+						item_name=malloc(parseBufLen);
+						memcpy(item_name,parseBuf,parseBufLen);
+						parseIndex=0;
+						itemIndex++;
 						continue;
 					}
 				}
-
-				value = config_value;
-				found_config_key = true;
+				if(c=='\n') {
+					isComment=false;
+					if(itemIndex==0) {
+						fprintf(stderr, "Syntax error: config file " _PATH_XCSELECT_CONF ": Invalid line %d, no value assigned!",lineNumber);
+						fclose(config_file);
+						xcselect_config.config_available=false;
+						free(item_name);
+						for(int i=0;i<configItemIndex;i++) {
+							free(config_entries[i][0]);
+							free(config_entries[i][1]);
+							free(config_entries[i]);
+						}
+						return;
+					}
+					itemIndex=0;
+					parseIndex=0;
+					lineNumber++;
+					int parseBufLen=strlen(parseBuf)+1;
+					char *item_value=malloc(parseBufLen);
+					memcpy(item_value,parseBuf,parseBufLen);
+					if(configItemIndex==configArrSize) {
+						config_entries=reallocarray(config_entries, configArrSize+32, sizeof(void*));
+					}
+					char **current_item=calloc(2,sizeof(char *));
+					current_item[0]=item_name;
+					current_item[1]=item_value;
+					config_entries[configItemIndex]=current_item;
+					item_name=NULL;
+					configItemIndex++;
+					continue;
+				}
+				if(isComment)
+					continue;
+				if(parseIndex==1024) {
+					fprintf(stderr, "Syntax error: Property name or value too long at line %d of config file " _PATH_XCSELECT_CONF ".", lineNumber);
+					fclose(config_file);
+					xcselect_config.config_available=false;
+					for(int i=0;i<configItemIndex;i++) {
+						free(config_entries[i][0]);
+						free(config_entries[i][1]);
+						free(config_entries[i]);
+					}
+					if(item_name!=NULL) {
+						free(item_name);
+					}
+					return;
+				}
+				parseBuf[parseIndex]=c;
 			}
+		} else {
+			// path_exists set errno to ENOENT if path not exist
+			fprintf(stderr, "Error: config file %s not exist, using default logics (%s)\n", _PATH_XCSELECT_CONF, strerror(errno));
 		}
-		fclose(xcselect_config.config_open);
 	}
-	return (xcselect_config.config_readable) ? found_config_key : false;
+	if(!xcselect_config.config_available) {
+		return NULL;
+	}
+	for(char ***item_group_ptr=xcselect_config.config_entries;item_group_ptr!=NULL;item_group_ptr++) {
+		char **item_group=*item_group_ptr;
+		if(strcmp(item_group[0], key)==0) {
+			return item_group[1];
+		}
+	}
+	return NULL;
 }
